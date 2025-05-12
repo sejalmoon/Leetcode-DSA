209cefe1bf639421bedd590242163ca0d25deef9
sejalmoon <72119918+sejalmoon@users.noreply.github.com>
2025-05-12 23:40:40 +0530

    Time: 0 ms (100.00%) | Memory: 46.3 MB (16.51%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,21 +1,44 @@
 class Solution {
-    public int compare(boolean state, int[] nums, int target) {
-        int n = nums.length, l = 0, r = n - 1, result = -1;
-        while (r >= l) {
-            int mid = (r + l) / 2;
-            if (nums[mid] == target) {
-                result = mid;
-                if (state) r = mid - 1;
-                else l = mid + 1;
-            } else if (nums[mid] > target) r = mid - 1;
-            else l = mid + 1;
-        }
-        return result;
-    }
-
     public int[] searchRange(int[] nums, int target) {
-        int s = compare(true, nums, target);
-        int e = compare(false, nums, target);
-        return new int[]{s, e};
+        int first=nums.length;
+        int last=-1;
+        int left=0;
+        int right=nums.length-1;
+        while(left<=right){
+            int mid = (left+right)/2;
+            if(nums[mid]==target){
+                first=Math.min(first,mid);
+                right=mid-1;
+            }
+            else if(nums[mid]>target){
+                right=mid-1;
+            }
+            else{
+                left=mid+1;
+            }
+        }
+        left=0;
+        right=nums.length-1;
+        while(left<=right){
+            int mid = (left+right)/2;
+            if(nums[mid]==target){
+                last=Math.max(last,mid);
+                left=mid+1;
+            }
+            else if(nums[mid]>target){
+                right=mid-1;
+            }
+            else{
+                left=mid+1;
+            }
+        }
+
+        if(first==nums.length){
+            int[] ans = {-1,-1};
+            return ans;
+        }
+        int[] ans = {first,last};
+        return ans;
+
     }
 }
