19a9da9c7da9868fef3775af807e2842041da6e7
sejalmoon <72119918+sejalmoon@users.noreply.github.com>
2025-05-19 23:15:32 +0530

    Time: 22 ms (33.36%) | Memory: 58.2 MB (65.54%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
@@ -1,41 +1,39 @@
 class Solution {
-
-    public boolean possible(int[] bloomDay, int day, int m, int k){
-        int n = bloomDay.length; // Size of the array
-        int cnt = 0;
-        int noOfB = 0;
-        for (int i = 0; i < n; i++) {
-            if (bloomDay[i] <= day) {
-                cnt++;
-            } else {
-                noOfB += (cnt / k);
-                cnt = 0;
+    public boolean possible(int[] bloomday, int mid, int m, int k){
+        int n = bloomday.length;
+        int count=0;
+        int bouno=0;
+        for(int i=0;i<n;i++){
+            if(bloomday[i]<=mid){
+                count++;
+            }
+            else{
+                bouno+=(count/k);
+                count=0;
             }
         }
-        noOfB+= (cnt/k);
-        return noOfB >= m;
+        bouno+=(count/k);
+        return bouno>=m;
     }
-
-
     public int minDays(int[] bloomDay, int m, int k) {
+        int low=Integer.MAX_VALUE;
+        int high=Integer.MIN_VALUE;
         long val = (long) m*k;
         int n=bloomDay.length;
         if(val>n){
             return -1;
         }
-        int mini = Integer.MAX_VALUE, maxi = Integer.MIN_VALUE;
-        for (int i = 0; i < n; i++) {
-            mini = Math.min(mini, bloomDay[i]);
-            maxi = Math.max(maxi, bloomDay[i]);
+        for(int i=0;i<n;i++){
+            low = Math.min(low, bloomDay[i]);
+            high=Math.max(high, bloomDay[i]);
         }
-        int low=mini;
-        int high=maxi;
         while(low<=high){
-            int mid = (low + high) / 2;
-            if (possible(bloomDay, mid, m, k)) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
+            int mid=(low+high)/2;
+            if(possible(bloomDay,mid,m,k)){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
             }
         }
         return low;
