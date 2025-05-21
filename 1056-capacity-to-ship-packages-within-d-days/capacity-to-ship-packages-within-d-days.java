c29b3fc3d2b7755cf5f5a11774bebb9ae83f6bc3
sejalmoon <72119918+sejalmoon@users.noreply.github.com>
2025-05-21 23:36:56 +0530

    Time: 10 ms (92.53%) | Memory: 48.1 MB (5.94%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
@@ -1,39 +1,27 @@
-import java.util.*;
 class Solution {
-    public static int shipWithinDays(int[] weights, int days) {
-        
-        int max=Integer.MIN_VALUE, s=0;
-        for(int x : weights)
-        {
-            max=Math.max(x,max);
-            s+=x;
+    public int shipWithinDays(int[] weights, int days) {
+        int maxWeight = -1, totalWeight = 0;
+        for (int weight : weights) {
+            maxWeight = Math.max(maxWeight, weight);
+            totalWeight += weight;
         }
-        int l=max,h=s,mid=0;
-        int dayReq=0;
-        while(l<=h)
-        {
-            mid=(l+h)/2;
-            dayReq=daysReq(weights,mid);
-            if(dayReq<=days)
-            h=mid-1;
-            else
-            l=mid+1;
+        int left = maxWeight, right = totalWeight;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            int daysNeeded = 1, currWeight = 0;
+            for (int weight : weights) {
+                if (currWeight + weight > mid) {
+                    daysNeeded++;
+                    currWeight = 0;
+                }
+                currWeight += weight;
+            }
+            if (daysNeeded > days) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
         }
-        return l;
-    }
-    public static int daysReq(int w[], int cap)
-    {
-        int load=0,d=1;
-        for(int i=0;i<w.length;i++)
-        {
-        if(load+w[i]>cap)
-        {
-            d+=1;
-            load=w[i];
-        }
-        else
-        load+=w[i];
-        }
-        return d;
+        return left;
     }
 }
