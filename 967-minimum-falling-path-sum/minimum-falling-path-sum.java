edfc2a0077bd1f433689c8931d1763dd46556e43
sejalmoon <72119918+sejalmoon@users.noreply.github.com>
2025-08-25 12:08:33 +0530

    Time: 9 ms (17.63%) | Memory: 45.8 MB (19.33%) - LeetSync

---
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -0,0 +1,50 @@
+class Solution {
+    private int solve(int[][] matrix, int i, int j, int n, int[][] dp){
+       if(j<0 || j>=n){
+        return Integer.MAX_VALUE;
+       }
+       if(i==0){
+        return dp[0][j]=matrix[0][j];
+       }
+       if(dp[i][j]!=Integer.MAX_VALUE){
+        return dp[i][j];
+       }
+       int s =  solve(matrix, i-1, j, n,dp);
+       int ld = solve(matrix, i-1, j-1, n, dp);
+       int rd = solve(matrix, i-1, j+1, n, dp);
+       return Integer.min(s, Integer.min(ld,rd)) + matrix[i][j];
+    }
+
+    public int minFallingPathSum(int[][] matrix) {
+        int n = matrix.length;
+        int dp[][] = new int[n][n];
+        for(int i=0;i<n;i++){
+                dp[0][i]=matrix[0][i];
+        }
+        for(int i=1;i<n;i++){
+            for(int j=0;j<n;j++){
+                int s = matrix[i][j] + dp[i-1][j];
+                int ld = matrix[i][j];
+                if(j-1>=0){
+                    ld += dp[i-1][j-1];
+                }
+                else{
+                    ld = Integer.MAX_VALUE;
+                }
+                int rd = matrix[i][j];
+                if(j+1<=n-1){
+                    rd += dp[i-1][j+1];
+                }
+                else{
+                    rd= Integer.MAX_VALUE;
+                }
+                dp[i][j] = Integer.min(s, Integer.min(ld,rd));
+            }   
+        }
+        int mini = dp[n-1][0];
+        for(int i=1;i<n;i++){
+                mini=Integer.min(mini,dp[n-1][i]);
+        }
+        return mini;
+    }
+}
