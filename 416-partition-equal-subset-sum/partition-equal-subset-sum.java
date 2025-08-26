aa6bc6549710a0ac34e615599dd03e61a8c0583f
sejalmoon <72119918+sejalmoon@users.noreply.github.com>
2025-08-26 11:09:08 +0530

    Time: 45 ms (60.11%) | Memory: 45.1 MB (71.33%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -30,8 +30,27 @@ class Solution {
             return false;
         }
         else{
-            Boolean dp[][] = new Boolean[n][(sum/2) + 1];
-            return solve(n-1, nums, sum/2, dp);
-        }    
+            int target = sum/2;
+            boolean dp[][] = new boolean[n][target + 1];
+            for(int i=0;i<n;i++){
+                dp[i][0] = true;
+            }
+            if(nums[0]<=target){
+                dp[0][nums[0]]=true;
+            }
+
+            for(int i=1;i<n;i++){
+                for(int j=1;j<=target;j++){
+                    boolean take = false;
+                    if(nums[i]<=j){
+                    take = dp[i-1][j-nums[i]];
+                    }
+                    boolean notTake = dp[i-1][j];
+                    dp[i][j] = take || notTake;
+                }
+            }
+        return dp[n-1][target]; 
+        }  
+     
     }
 }
