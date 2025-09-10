e450496588266f8b15cae32f58ae6c321dfde189
sejalmoon <72119918+sejalmoon@users.noreply.github.com>
2025-09-10 09:32:47 +0530

    Time: 40 ms (66.41%) | Memory: 44.6 MB (43.50%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,42 +1,19 @@
 class Solution {
-    public int lengthOfLIS(int[] nums) {
-       int n = nums.length;
-       int dp[][] = new int[n+1][n+1];
-       for(int i=0;i<=n;i++){
-        for(int j=0;j<=n;j++){
-            dp[i][j]=0;
+    public int lengthOfLIS(int[] arr) {
+       int n = arr.length;
+        int dp[] = new int[n];
+        for(int i=0;i<n;i++){
+            dp[i]=1;
         }
-       }
-
-       for(int ind=n-1;ind>=0;ind--){
-        for(int prevIndex=ind-1;prevIndex>=-1;prevIndex--){
-            int notTake = dp[ind+1][prevIndex+1];
-            int take = 0;
-            if(prevIndex==-1 || nums[ind]>nums[prevIndex]){
-                take = 1 + dp[ind+1][ind+1];
+        int maxi=1;
+        for(int i=0;i<n;i++){
+            for(int prev=0;prev<i;prev++){
+                if(arr[prev]<arr[i]){
+                    dp[i]=Math.max(1+dp[prev],dp[i]);
+                }
             }
-            dp[ind][prevIndex+1] = Math.max(take,notTake);
+            maxi=Math.max(maxi,dp[i]);
         }
-       }
-
-       return dp[0][0];
-    }
-
-    private int solve(int index, int n, int[] nums, int prevIndex, int dp[][]){
-        if(index==n){
-            return 0;
-        }
-
-        if(dp[index][prevIndex+1]!=-1){
-            return dp[index][prevIndex+1];
-        }
-
-        int take = 0;
-        if(prevIndex==-1 || nums[index]>nums[prevIndex]){
-            take = 1 + solve(index+1,n,nums,index,dp);
-        }
-        int notTake = solve(index+1,n,nums,prevIndex,dp);
-
-        return dp[index][prevIndex+1]=Math.max(take,notTake);
-    }
+        return maxi;
+    }    
 }
