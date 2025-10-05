550a87683ef1baad0b5f81b4ca58be04b41c4d9e
sejalmoon <72119918+sejalmoon@users.noreply.github.com>
2025-10-05 20:39:13 +0530

    Time: 51 ms (27.35%) | Memory: 47.6 MB (56.51%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
@@ -1,27 +1,24 @@
 class Solution {
     public int subarraysWithKDistinct(int[] nums, int k) {
-        return subarrayWithAtMostK(nums, k) - subarrayWithAtMostK(nums, k - 1);
+        return f(nums,k)-f(nums,k-1);
     }
-    
-    public int subarrayWithAtMostK(int[] nums, int k) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int left = 0, right = 0, ans = 0;
-        
-        while (right < nums.length) {
-            map.put(nums[right], map.getOrDefault(nums[right], 0) + 1);
-            
-            while (map.size() > k) {
-                map.put(nums[left], map.get(nums[left]) - 1);
-                if (map.get(nums[left]) == 0) {
-                    map.remove(nums[left]);
+
+    public int f(int[] nums, int k){
+        int l=0, r=0, count=0;
+        Map<Integer,Integer> mpp=new HashMap<>();
+        while(r<nums.length){
+            mpp.put(nums[r], mpp.getOrDefault(nums[r],0)+1);
+
+            while(mpp.size()>k){
+                mpp.put(nums[l], mpp.get(nums[l])-1);
+                if(mpp.get(nums[l])==0){
+                    mpp.remove(nums[l]);
                 }
-                left++;
+                l++;
             }
-            
-            ans += right - left + 1; 
-            right++;
+            count+=r-l+1;
+            r++;
         }
-        
-        return ans;
+        return count;
     }
 }
