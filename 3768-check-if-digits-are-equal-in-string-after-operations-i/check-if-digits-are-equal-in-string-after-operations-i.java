39dc55650b701971b65be584305eb1231e0fb1f6
sejalmoon <72119918+sejalmoon@users.noreply.github.com>
2025-10-23 18:26:57 +0530

    Time: 8 ms (72.03%) | Memory: 44.8 MB (49.68%) - LeetSync

---
+++ 3768-check-if-digits-are-equal-in-string-after-operations-i/check-if-digits-are-equal-in-string-after-operations-i.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean hasSameDigits(String s) {
+        if(s.length()==2){
+            return s.charAt(0)==s.charAt(1);
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<s.length()-1;i++){
+            sb.append(((int)s.charAt(i)+(int)s.charAt(i+1))%10);
+        }
+        return hasSameDigits(sb.toString());
+    }
+}
