9fb1d7e9fd90b46086a8fb7da95b7aa6729f9361
sejalmoon <72119918+sejalmoon@users.noreply.github.com>
2025-10-26 14:53:52 +0530

    Time: 6 ms (23.08%) | Memory: 52 MB (23.96%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -7,7 +7,7 @@ class Solution {
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]=='1' && vis[i][j]==0){
-                    dfs(i, j, grid, vis);
+                    bfs(i, j, grid, vis);
                     count++;
                 }
             }
@@ -25,5 +25,25 @@ class Solution {
         dfs(row,col-1,grid,vis);
         dfs(row+1,col,grid,vis);
         dfs(row,col+1,grid,vis);        
+    }
+
+    private void bfs(int row, int col, char[][] grid, int[][] vis){
+        int[][] directions = {{1,0}, {-1,0}, {0,1}, {0,-1}};
+        vis[row][col]=1;
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{row,col});
+
+        while(!q.isEmpty()){
+            int[] front = q.poll();
+            for(int[] d:directions){
+                int r = front[0]+d[0];
+                int c = front[1]+d[1];
+                if(r>=0 && r<grid.length && c>=0 && c<grid[0].length && grid[r][c]=='1' && vis[r][c]==0){
+                    vis[r][c]=1;
+                    q.add(new int[]{r,c});
+                }
+            }
+
+        }
     }
 }
