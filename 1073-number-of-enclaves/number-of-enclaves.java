9ab5e950ce8eb883434e04e4d81f0041ab205629
sejalmoon <72119918+sejalmoon@users.noreply.github.com>
2025-11-03 21:08:30 +0530

    Time: 9 ms (55.48%) | Memory: 55.4 MB (64.91%) - LeetSync

---
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int numEnclaves(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        int[][] vis = new int[n][m];
+        Queue<int []> q = new LinkedList<>();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){                
+                if(i==0 || j==0 || i==n-1 || j==m-1){
+                    if(grid[i][j]==1){
+                        q.add(new int[]{i,j});
+                        vis[i][j] = 1;
+                    }
+                }
+            }
+        }
+
+        int[] delrow = {-1,0,1,0};
+        int[] delcol = {0,1,0,-1};
+        while(!q.isEmpty()){
+            int row = q.peek()[0];
+            int col = q.peek()[1];
+            q.remove();
+            
+            for(int i=0;i<4;i++){
+                int nrow = row + delrow[i];
+                int ncol = col + delcol[i];
+
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]==1 && vis[nrow][ncol]==0){
+                    q.add(new int[]{nrow, ncol});
+                    vis[nrow][ncol] = 1;
+                }
+            }
+        }
+
+        int count = 0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==1 && vis[i][j]==0){
+                    count++;
+                }
+            }
+        }
+
+        return count;
+
+    }
+}
