352347edf97fd9c5f8104109e2f86f8666793d91
sejalmoon <72119918+sejalmoon@users.noreply.github.com>
2025-12-06 15:25:13 +0530

    Time: 5 ms (11.58%) | Memory: 47.1 MB (75.71%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,25 +1,77 @@
+class DisjointSet{
+    List<Integer> rank = new ArrayList<>();
+    List<Integer> parent = new ArrayList<>();
+    List<Integer> size = new ArrayList<>();
+
+    public DisjointSet(int n){
+        for(int i = 0; i<n; i++){
+            parent.add(i);
+            size.add(1);
+            rank.add(0);
+        }
+    }
+
+    public int findParent(int node){
+        if(parent.get(node)!=node){
+            parent.set(node, findParent(parent.get(node)));
+        }
+        return parent.get(node);
+    }
+
+    public void UnionByRank(int u, int v){
+        int p1 = findParent(u);
+        int p2 = findParent(v);
+        if(p1 == p2){
+            return;
+        }
+        if(rank.get(p1) < rank.get(p2)){
+            parent.set(p1, p2);
+        }
+        else if(rank.get(p1) > rank.get(p2)){
+            parent.set(p2, p1);
+        }
+        else{
+            parent.set(p2, p1);
+            rank.set(p1, rank.get(p1)+1);
+        }
+    }
+
+    public void UnionBySize(int u, int v){
+        int p1 = findParent(u);
+        int p2 = findParent(v);
+        if(p1 == p2){
+            return;
+        }
+        if(size.get(p1) < size.get(p2)){
+            parent.set(p1, p2);
+            size.set(p2, size.get(p1) + size.get(p2));
+        }
+        else{
+            parent.set(p2, p1);
+            size.set(p1, size.get(p1) + size.get(p2));
+        }
+    }
+}
+
+
 class Solution {
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
-        boolean[] vis = new boolean[n];
-        int count = 0;
-
+        DisjointSet ds = new DisjointSet(n);
         for(int i = 0; i<n; i++){
-            if(!vis[i]){
-                dfs(i, n, vis, isConnected);
+            for(int j = 0; j<n; j++){
+                if(isConnected[i][j] == 1 & i!=j){
+                    ds.UnionByRank(i,j);
+                }
+            }
+        }
+
+        int count = 0;
+        for(int i = 0; i<n; i++){
+            if(ds.parent.get(i) == i){
                 count++;
             }
         }
-
         return count;
-    }
-
-    private void dfs(int ind, int n, boolean[] vis, int[][] isConnected){
-        vis[ind]=true;
-        for(int i=0; i<n; i++){
-            if(isConnected[ind][i]==1 && !vis[i]){
-                dfs(i, n, vis, isConnected);
-            }
-        }
     }
 }
