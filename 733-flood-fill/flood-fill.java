1093b5cbd5886f9703bf5dbb2c0d2ac0ac7bfdcb
sejalmoon <72119918+sejalmoon@users.noreply.github.com>
2026-03-28 16:42:26 +0530

    Time: 0 ms (100.00%) | Memory: 46.7 MB (73.37%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,20 +1,23 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int inicolor = image[sr][sc];
+        int oriColor = image[sr][sc];
         int[][] ans = image;
-        int[] delrow = {-1,0,1,0};
-        int[] delcol = {0,1,0,-1};
-        dfs(sr,sc,color,ans,delrow,delcol,inicolor,image);
+        dfs(ans, sr, sc, oriColor, color, image);
         return ans;
     }
 
-    private void dfs(int row, int col, int color, int[][] ans, int[] delrow, int[] delcol, int inicolor, int[][] image){
-        ans[row][col] = color;
-        for(int i=0;i<4;i++){
-            int nrow = row + delrow[i];
-            int ncol = col + delcol[i];
-            if(nrow>=0 && nrow<ans.length && ncol>=0 && ncol<ans[0].length && image[nrow][ncol]==inicolor && ans[nrow][ncol]!=color){
-                dfs(nrow,ncol,color,ans,delrow,delcol,inicolor,image);
+    private void dfs(int[][] ans, int sr, int sc, int oriColor, int color, int[][] image){
+        int m = ans.length;
+        int n = ans[0].length;
+        int[] delrow = {-1, 0, 1, 0};
+        int[] delcol = {0, -1, 0, 1};
+
+        ans[sr][sc] = color;
+        for(int i = 0; i<4; i++){
+            int nrow = sr + delrow[i];
+            int ncol = sc + delcol[i];
+            if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && image[nrow][ncol]==oriColor && ans[nrow][ncol] != color){
+                dfs(ans, nrow, ncol, oriColor, color, image);
             }
         }
     }
