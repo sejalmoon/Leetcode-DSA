0e373ce841fa0b3c36ba5255a715a1abe74b4039
sejalmoon <72119918+sejalmoon@users.noreply.github.com>
2026-03-29 19:39:28 +0530

    Time: 9 ms (51.12%) | Memory: 63.7 MB (12.47%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.java
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -1,48 +1,48 @@
 class Solution {
     public int numEnclaves(int[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
-        int[][] vis = new int[n][m];
-        Queue<int []> q = new LinkedList<>();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){                
-                if(i==0 || j==0 || i==n-1 || j==m-1){
-                    if(grid[i][j]==1){
-                        q.add(new int[]{i,j});
-                        vis[i][j] = 1;
-                    }
-                }
+        int m = grid.length;
+        int n = grid[0].length;
+
+        boolean[][] vis = new boolean[m][n];
+        for(int i = 0; i<m; i++){
+            if(grid[i][0] == 1){
+                dfs(i, 0, grid, vis, m, n);
+            }
+            if(grid[i][n-1] == 1){
+                dfs(i, n-1, grid, vis, m, n);
             }
         }
 
-        int[] delrow = {-1,0,1,0};
-        int[] delcol = {0,1,0,-1};
-        while(!q.isEmpty()){
-            int row = q.peek()[0];
-            int col = q.peek()[1];
-            q.remove();
-            
-            for(int i=0;i<4;i++){
-                int nrow = row + delrow[i];
-                int ncol = col + delcol[i];
-
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && grid[nrow][ncol]==1 && vis[nrow][ncol]==0){
-                    q.add(new int[]{nrow, ncol});
-                    vis[nrow][ncol] = 1;
-                }
+        for(int i = 0; i<n; i++){
+            if(grid[0][i] == 1){
+                dfs(0, i, grid, vis, m, n);
+            }
+            if(grid[m-1][i] == 1){
+                dfs(m-1, i, grid, vis, m, n);
             }
         }
 
         int count = 0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==1 && vis[i][j]==0){
+        for(int i = 0; i<m; i++){
+            for(int j = 0; j<n; j++){
+                if(grid[i][j] == 1 && !vis[i][j]){
                     count++;
                 }
             }
         }
-
         return count;
+    }
 
+    private void dfs(int row, int col, int[][] grid, boolean[][] vis, int m, int n){
+        vis[row][col] = true;
+        int[] delrow = {-1, 0, 1, 0};
+        int[] delcol = {0, -1, 0, 1};
+        for(int i = 0; i<4; i++){
+            int nrow = row + delrow[i];
+            int ncol = col + delcol[i];
+            if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && grid[nrow][ncol] == 1 && !vis[nrow][ncol]){
+                dfs(nrow, ncol, grid, vis, m, n);
+            }
+        }
     }
 }
