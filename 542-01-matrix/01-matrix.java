2c6794e88899c1b534537bb4af875cc4c5ed6463
sejalmoon <72119918+sejalmoon@users.noreply.github.com>
2026-03-29 22:40:15 +0530

    Time: 16 ms (29.39%) | Memory: 48.9 MB (77.60%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -1,50 +1,49 @@
 class Solution {
+
     class Node{
         int i;
         int j;
         int steps;
-        Node(int i, int j, int steps){
+        public Node(int i, int j, int steps){
             this.i = i;
             this.j = j;
             this.steps = steps;
         }
     }
     public int[][] updateMatrix(int[][] mat) {
-        int n = mat.length;
-        int m = mat[0].length;
-        Queue<Node> q = new LinkedList<>(); 
-        boolean[][] isVisited = new boolean[n][m];
-        int[][] result = new int[n][m];
-        
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(mat[i][j]==0){
-                    q.add(new Node(i, j, 0)); 
-                    isVisited[i][j] = true; 
+        int m = mat.length;
+        int n = mat[0].length;
+
+        boolean[][] vis = new boolean[m][n];
+        Queue<Node> q = new LinkedList<>();
+        int[][] ans = new int[m][n];
+        for(int i = 0; i<m; i++){
+            for(int j = 0; j<n; j++){
+                if(mat[i][j] == 0){
+                    q.add(new Node(i, j, 0));
+                    vis[i][j] = true;
                 }
             }
         }
-        
-        int[][] dir = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
-        
+
         while(!q.isEmpty()){
-            Node curr = q.poll(); 
-            int row = curr.i;
-            int col = curr.j;
-            int steps = curr.steps;
+            Node a = q.poll();
+            int row = a.i;
+            int col = a.j;
+            int steps = a.steps;
 
-            result[row][col] = steps; 
-
-            for(int i=0; i<4; i++){
-                int nrow = row + dir[i][0];
-                int ncol = col + dir[i][1];
-                
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && !isVisited[nrow][ncol]){
-                    isVisited[nrow][ncol] = true; 
-                    q.add(new Node(nrow, ncol, steps+1)); 
+            int[] delrow = {-1, 0, 1, 0};
+            int[] delcol = {0, -1, 0, 1};
+            for(int i = 0; i<4; i++){
+                int nrow = row + delrow[i];
+                int ncol = col + delcol[i];
+                if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && !vis[nrow][ncol]){
+                    q.add(new Node(nrow, ncol, steps+1));
+                    ans[nrow][ncol] = steps + 1;
+                    vis[nrow][ncol] = true;
                 }
             }
         }
-        return result; 
+        return ans;
     }
 }
