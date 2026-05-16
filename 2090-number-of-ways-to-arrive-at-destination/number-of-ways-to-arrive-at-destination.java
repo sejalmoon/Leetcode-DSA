3d6d713f5c3a5156b6f0ed982f90bfe8dbe2ef92
sejalmoon <72119918+sejalmoon@users.noreply.github.com>
2026-05-16 15:13:22 +0530

    Time: 14 ms (46.98%) | Memory: 52.1 MB (23.89%) - LeetSync

--- 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.java
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.java
@@ -1,7 +1,7 @@
 class Pair{
     int node;
     long wt;
-    Pair(int node, long wt){
+    public Pair(int node, long wt){
         this.node = node;
         this.wt = wt;
     }
@@ -9,46 +9,49 @@ class Pair{
 
 class Solution {
     public int countPaths(int n, int[][] roads) {
-        List<List<Pair>> adjList = new ArrayList<>();
-        for(int i=0; i<n; i++){
-            adjList.add(new ArrayList<>());
+        List<List<Pair>> adj = new ArrayList<>();
+        for(int i = 0; i<n; i++){
+            adj.add(new ArrayList<>());
         }
 
         for(int i = 0; i<roads.length; i++){
-            adjList.get(roads[i][0]).add(new Pair(roads[i][1], roads[i][2]));
-            adjList.get(roads[i][1]).add(new Pair(roads[i][0], roads[i][2]));
+            adj.get(roads[i][0]).add(new Pair(roads[i][1], roads[i][2]));
+            adj.get(roads[i][1]).add(new Pair(roads[i][0], roads[i][2]));
         }
 
+        int mod = (int)(1e9 + 7);
+        PriorityQueue<Pair> pq = new PriorityQueue<>((x, y) -> Long.compare(x.wt, y.wt));
+        pq.add(new Pair(0, 0));
         long[] dist = new long[n];
         int[] ways = new int[n];
+
         for(int i = 0; i<n; i++){
             dist[i] = Long.MAX_VALUE;
         }
-
         dist[0] = 0;
         ways[0] = 1;
-        PriorityQueue<Pair> pq = new PriorityQueue<>((x, y) -> Long.compare(x.wt, y.wt));
-        pq.add(new Pair(0,0));
-        int mod = (int)(1e9 + 7);
-        while(pq.size() != 0){
-            int node = pq.peek().node;
-            long wt = pq.peek().wt;
+
+        while(!pq.isEmpty()){
+            Pair p = pq.peek();
+            int node = p.node;
+            long wt = p.wt;
             pq.remove();
 
-            for(Pair it:adjList.get(node)){
-                int adjNode = it.node;
-                long edgewt = it.wt;
+            for(Pair it:adj.get(node)){
+                int newNode = it.node;
+                long newWt = it.wt;
 
-                if(wt + edgewt < dist[adjNode]){
-                    dist[adjNode] = wt + edgewt;
-                    ways[adjNode] = ways[node];
-                    pq.add(new Pair(adjNode, dist[adjNode]));
+                if(newWt + wt < dist[newNode]){
+                    dist[newNode] = newWt + wt;
+                    ways[newNode] = ways[node];
+                    pq.add(new Pair(newNode, dist[newNode]));
                 }
-                else if(wt + edgewt == dist[adjNode]){
-                    ways[adjNode] = (ways[adjNode] + ways[node])%mod;
+                else if(newWt + wt == dist[newNode]){
+                    ways[newNode] = (ways[newNode] + ways[node])%mod;
                 }
             }
         }
+
         return ways[n-1]%mod;
     }
 }
