3dc304bfe03e9eb8080015f0648075ee26c5b091
sejalmoon <72119918+sejalmoon@users.noreply.github.com>
2026-06-03 23:58:58 +0530

    Time: 125 ms (13.05%) | Memory: 198.2 MB (96.10%) - LeetSync

--- 1300-critical-connections-in-a-network/critical-connections-in-a-network.java
+++ 1300-critical-connections-in-a-network/critical-connections-in-a-network.java
@@ -1,44 +1,45 @@
 class Solution {
+    int timer = 1;
     public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) {
         List<List<Integer>> adj = new ArrayList<>();
-        for(int i=0; i<n; i++){
+        for(int i = 0; i<n; i++){
             adj.add(new ArrayList<>());
         }
-        for(int i=0; i<connections.size(); i++){
-            int u = connections.get(i).get(0);
-            int v = connections.get(i).get(1);
-            adj.get(u).add(v);
-            adj.get(v).add(u);
+
+        for(int i = 0; i<connections.size(); i++){
+            adj.get(connections.get(i).get(0)).add(connections.get(i).get(1));
+            adj.get(connections.get(i).get(1)).add(connections.get(i).get(0));
         }
 
-        int vis[] = new int[n];
-        int low[] = new int[n];
-        int step[] = new int[n];
+        int[] vis = new int[n];
+        int[] low = new int[n];
+        int[] tin = new int[n];
+
         List<List<Integer>> ans = new ArrayList<>();
-
-        dfs(0, -1, vis, adj, step, low, ans);
-
-        return ans;
+        dfs(0, -1, adj, vis, low, tin, ans);
+        return ans;        
     }
 
-    static int timer=1;
-    void dfs(int node,int parent,int vis[],List<List<Integer>> adj,int step[],int low[],List<List<Integer>> ans){
-        vis[node]=1;
-        step[node]=timer;
-        low[node]=timer;
+    private void dfs(int node, int parent, List<List<Integer>> adj, int[] vis, int[] low, int[] tin, List<List<Integer>> ans){
+        vis[node] = 1;
+        low[node] = timer;
+        tin[node] = timer;
         timer++;
-        for(int i:adj.get(node)){
-            if(i==parent)continue;
-            if(vis[i]==0){
-                dfs(i,node,vis,adj,step,low,ans);
-                low[node]=Math.min(low[node],low[i]);
-                if(low[i]>step[node]){
-                    ans.add(List.of(node, i));
+
+        for(Integer it:adj.get(node)){
+            if(it == parent) continue;
+
+            if(vis[it] == 0){
+                dfs(it, node, adj, vis, low, tin, ans);
+                low[node] = Math.min(low[node], low[it]);
+                if(tin[node] < low[it]){
+                    ans.add(Arrays.asList(it, node));
                 }
-            }
-            else{
-                low[node]=Math.min(low[node],low[i]);
+                
+            }else{
+                low[node] = Math.min(low[node], low[it]);
             }
         }
+
     }
 }
