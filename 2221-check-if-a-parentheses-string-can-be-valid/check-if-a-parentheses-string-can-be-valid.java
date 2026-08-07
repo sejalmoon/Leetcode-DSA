d0eef7fdb33ad3fd872a7169c8c984e8f62f7ba9
sejalmoon <72119918+sejalmoon@users.noreply.github.com>
2026-08-07 23:12:21 +0530

    Time: 25 ms (11.90%) | Memory: 49.7 MB (17.38%) - LeetSync

---
+++ 2221-check-if-a-parentheses-string-can-be-valid/check-if-a-parentheses-string-can-be-valid.java
@@ -0,0 +1,43 @@
+class Solution {
+
+    public boolean canBeValid(String s, String locked) {
+        int length = s.length();
+        if (length % 2 == 1) {
+            return false;
+        }
+
+        Stack<Integer> open = new Stack<>();
+        Stack<Integer> unlocked = new Stack<>();
+
+        for (int i = 0; i < length; i++) {
+            if (locked.charAt(i) == '0') {
+                unlocked.push(i);
+            } else if (s.charAt(i) == '(') {
+                open.push(i);
+            } else if (s.charAt(i) == ')') {
+                if (!open.empty()) {
+                    open.pop();
+                } else if (!unlocked.empty()) {
+                    unlocked.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        while (
+            !open.empty() &&
+            !unlocked.empty() &&
+            open.peek() < unlocked.peek()
+        ) {
+            open.pop();
+            unlocked.pop();
+        }
+
+        if (!open.empty()) {
+            return false;
+        }
+
+        return true;
+    }
+}
