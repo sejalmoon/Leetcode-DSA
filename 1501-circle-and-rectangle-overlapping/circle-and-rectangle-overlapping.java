c01f52a8707900c5f10a0906002738649f9666f2
sejalmoon <72119918+sejalmoon@users.noreply.github.com>
2026-09-19 14:54:13 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (60.50%) - LeetSync

--- 1501-circle-and-rectangle-overlapping/circle-and-rectangle-overlapping.java
+++ 1501-circle-and-rectangle-overlapping/circle-and-rectangle-overlapping.java
@@ -1,27 +1,15 @@
 class Solution {
     public boolean checkOverlap(int radius, int xcenter, int ycenter, int x1, int y1, int x2, int y2) {
-        if(xcenter >= x1 && xcenter <= x2 && ycenter >= y1 && ycenter <= y2) return true;
+        double dist = 0;
+        if(xcenter < x1 || xcenter > x2){
+            dist += Math.min(Math.pow(x1 - xcenter, 2), Math.pow(x2 - xcenter, 2));
+        }
 
-        if(xcenter >= x1 && xcenter <= x2 && ycenter >= y2 && ycenter <= y2 + radius) return true;
+        if(ycenter < y1 || ycenter > y2){
+            dist += Math.min(Math.pow(y1 - ycenter, 2), Math.pow(y2 - ycenter, 2));
+        }
 
-        if(xcenter >= x1 && xcenter <= x2 && ycenter <= y1 && ycenter >= y1 - radius) return true;
-
-        if(xcenter <= x1 && xcenter >= x1 - radius && ycenter >= y1 && ycenter <= y2) return true;
-
-        if(xcenter >= x2 && xcenter <= x2 + radius && ycenter >= y1 && ycenter <= y2) return true;
-
-        if(dist(xcenter, ycenter, x1, y1) <= radius*radius) return true;
-
-        if(dist(xcenter, ycenter, x2, y1) <= radius*radius) return true;
-
-        if(dist(xcenter, ycenter, x2, y2) <= radius*radius) return true;
-
-        if(dist(xcenter, ycenter, x1, y2) <= radius*radius) return true;
-
-        return false;
+        return dist <= radius*radius;
     }
 
-    private long dist(int x1, int y1, int x2, int y2){
-        return (long)(Math.pow(x1 - x2, 2)) + (long)(Math.pow(y1 - y2, 2));
-    }
 }
